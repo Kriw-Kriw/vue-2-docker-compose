e23df9817f226a3db55a5b8cdbe3552c01074444
Kriw-Kriw <102183495+Kriw-Kriw@users.noreply.github.com>
2024-12-18 00:43:33 +0300

    ЛР №3: Игра Алхимик
    ФИО: Кривов Д.А.
    
    Задача: 
    Необходимо реализовать крафт-игру “Алхимик”. Наверняка знаете или слышали.
    Есть список открытых предметов (будем выводить в верхней части экрана 80% поля сеткой). Рассчитываем под мобильный экран. В нижней части стол. С поля выбираются предметы на стол (кликом). На столе можно увеличитьва их количество (сложный вариант). Справа от стола, две кнопки, сбросить и смешать. Если формула предметов подобрана верно, то открывается новый предмент на стол, с которым можнно дальше смешивать.

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <PageLayout>
+      <section class="p-16">
+        Homepage
+        <button @click="() => openHelpModal()">Open modal</button>
+      </section>
+      <section>
+        <GridLayout />
+      </section>
+    </PageLayout>
+</template>
+
+<script>
+import PageLayout from '../parts/PageLayout'
+import GridLayout from "../grid/GridLayout";
+import { helpModal } from "@/mixins/modals";
+
+export default {
+  name: 'HomePage',
+  mixins: [helpModal],
+  components: {
+    PageLayout,
+    GridLayout
+  }
+}
+</script>
+
+<style>
+</style>
