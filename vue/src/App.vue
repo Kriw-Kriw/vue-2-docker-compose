b2932e2759da6fde061a91b74c2670b745a7c112
Kriw-Kriw <102183495+Kriw-Kriw@users.noreply.github.com>
2025-01-17 17:02:37 +0300

    Лабораторная работа №3: Алхимик
    ФИО: Кривов Д.А.
    
    Видео работы:
    https://github.com/user-attachments/assets/48790aec-9b0e-4a92-826f-c39db5f79b8b
    
    Задача:
    Необходимо реализовать крафт-игру “Алхимик”. Наверняка знаете или слышали.
    Есть список открытых предметов (будем выводить в верхней части экрана 80% поля сеткой). Рассчитываем под мобильный экран. В нижней части стол. С поля выбираются предметы на стол (кликом). На столе можно увеличивать их количество (сложный вариант). Справа от стола, две кнопки, сбросить и смешать. Если формула предметов подобрана верно, то открывается новый предмет на стол, с которым можно дальше смешивать.

---
+++ vue/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+    <RouterView/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+    body {
+        padding: 0;
+        margin: 0;
+    }
+</style>
